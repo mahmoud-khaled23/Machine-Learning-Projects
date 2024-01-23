e08ed0049aea8b4a57990c4d0ef580505e311fbf
mahmoud-khaled01 <mahmoudkhaled1116@gmail.com>
2024-01-23 21:31:14 +0200

    add Fraud Detection

---
+++ Fraud_Detection/baseline/baseline.py
@@ -0,0 +1,15 @@
+"""
+baseline model for Fraud Detection
+"""
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# import imblearn
+
+if __name__ == '__main__':
+    x = 0
+
+    print(f'Fraud Detection works pretty well')
+
